ffb3ecbcf2f16a386f9fa926890405e11ea6fac8
paoGelvez <pgelvezandrea@gmail.com>
2022-10-10 14:27:02 -0500

    the index function and the create function of the wallet and the user were created

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -3,8 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Wallet;
 
 class WalletController extends Controller
 {
-    //
+    function index(){
+
+        try{
+
+            $wallet = Wallet::all();
+
+            if(!$wallet){
+                return 'no hay cartera';
+            }else{
+                return response()->json($wallet);
+            }
+
+        }catch(\Exception $e){
+            return response()->json([
+                'error'=> $e->getMessage()
+            ],404);
+        }
+
+        
+    }
+
+
+    function create(Request $request ){
+        try{
+            $request -> validate([
+                'current_amount'=>'required|numeric',
+                'id_user'=>'required|integer'
+
+            ]);
+            $wallet = Wallet::create([
+
+                'current_amount'=> $request->current_amount,
+                'id_user'=> $request->id_user
+
+            ]);
+
+            return response()->json($wallet);
+        }catch(\Exception $e){
+            return response()->json([
+                'error'=> $e->getMessage()
+            ], 404);
+        }
+    }
 }
