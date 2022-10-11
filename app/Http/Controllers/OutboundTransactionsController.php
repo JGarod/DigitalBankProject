1d3b288d3a51e1386f4e0516e6d4cc657ffe30e3
Naifer <naiferdavid@hotmail.com>
2022-10-11 15:57:08 -0500

    clonar DG-7 con información de historial

--- app/Http/Controllers/OutboundTransactionsController.php
+++ app/Http/Controllers/OutboundTransactionsController.php
@@ -6,30 +6,7 @@ use Illuminate\Http\Request;
 
 class OutboundTransactionsController extends Controller
 {
-    public function create(Request $request)
-    {
-        try {
-            $request->validate([
-                'receive_wallet_id'=>'required|integer',
-                'send_wallet_id'=>'required|integer',
-                'outbound_amount'=>'required|numeric'
-            ]);
-        
-            $outbound = Outbound_Transactions::create([
-                'receive_wallet_id'=>$request->receive_wallet_id,
-                'send_wallet_id'=>$request->send_wallet_id,
-                'outbound_amount'=>$request->outbound_amount
-            ]);
-        
-            return response()->json([
-                'outbound' => $outbound
-            ]);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'error' => $e->getMessage()
-            ],400);
-        }
-    }
+    
 }
 
 
