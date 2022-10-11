26e519fc5c21bd9597adebf780c6f0973e8df1c4
KDOE-117 <jdn2037@gmail.com>
2022-10-11 00:33:22 -0500

    Commit about the send and receive amount of the cash in the wallet

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+
 
 class UserController extends Controller
 {
-    //
+    function create(Request $request){
+        try{
+            $request->validate([
+                'NIT'=>'required|numeric',
+                'email'=>'required|string',
+                'password'=>'required|string',
+                'name'=>'required|string',
+                'phone'=>'required|string'
+            ]);
+            $user = User::create([
+                'NIT'=> $request -> NIT,
+                'email'=> $request -> email,
+                'password'=> $request -> password,
+                'name'=> $request -> name ,
+                'phone'=> $request -> phone
+            ]);
+            return response()->json($user);
+        }catch(\Exception $e){
+            return response()->json([
+                'error'=> $e-> getMessage()
+            ],404);
+        }
+ 
+    }
+
 }
